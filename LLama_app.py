e55b111b03ecc93ad41109d74ef1d4eb36da4695
V Guruprasad <162950204+VGuruprasad2386@users.noreply.github.com>
2025-04-15 13:01:03 +0530

    Update LLama_app.py

--- LLama_app.py
+++ LLama_app.py
@@ -1,6 +1,5 @@
 import os
 import streamlit as st
-from dotenv import load_dotenv
 from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
